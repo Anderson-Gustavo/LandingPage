de85e623b8e499e797379f2ace38d3e08cfe61bb
Anderson-Gustavo <gustavodliimar@gmail.com>
2022-03-15 16:29:56 -0300

    declarando as variáveis no JS

---
+++ Landingpage/index.js
@@ -0,0 +1,4 @@
+var community = document.getElementById('a1')
+var tutorial = document.getElementById('a2')
+var openSource = document.getElementById('a3')
+var comands = document.getElementById('a4')
