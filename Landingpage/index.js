e885e3801a3f4204fe90e51b4a0a48c011ad682a
Anderson-Gustavo <gustavodliimar@gmail.com>
2022-03-15 16:49:32 -0300

    adicionando animação na barra de navegãção utilizando o JS

--- Landingpage/index.js
+++ Landingpage/index.js
@@ -2,3 +2,42 @@ var community = document.getElementById('a1')
 var tutorial = document.getElementById('a2')
 var openSource = document.getElementById('a3')
 var comands = document.getElementById('a4')
+
+community.addEventListener('mouseenter', () => {
+    community.style.backgroundColor = 'blue'
+    community.style.width = '200px' 
+})
+community.addEventListener('mouseout', () => {
+    community.style.backgroundColor = ''
+    community.style.width = ''
+})
+
+
+tutorial.addEventListener('mouseenter', () => {
+    tutorial.style.backgroundColor = 'blue'
+    tutorial.style.width = '100px' 
+})
+tutorial.addEventListener('mouseout', () => {
+    tutorial.style.backgroundColor = ''
+    tutorial.style.width = ''
+})
+
+
+openSource.addEventListener('mouseenter', () => {
+    openSource.style.backgroundColor = 'blue'
+    openSource.style.width = '120px' 
+})
+openSource.addEventListener('mouseout', () => {
+    openSource.style.backgroundColor = ''
+    openSource.style.width = ''
+})
+
+
+comands.addEventListener('mouseenter', () => {
+    comands.style.backgroundColor = 'blue'
+    comands.style.width = '120px' 
+})
+comands.addEventListener('mouseout', () => {
+    comands.style.backgroundColor = ''
+    comands.style.width = ''
+})
